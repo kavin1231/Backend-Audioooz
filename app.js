648af2ccc501c274abeed16fe4a7e9350adf9872
kavin1231 <chamathkaveendra@gmail.com>
2025-06-30 01:40:53 +0530

    Add dotenv configuration to app.js for environment variable management

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
