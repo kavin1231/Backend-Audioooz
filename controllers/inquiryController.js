4c0071c58a9e7715ca269bef73935b86471d3949
kavin1231 <chamathkaveendra@gmail.com>
2025-07-02 01:03:42 +0530

    Implement inquiry management features: add getInquiries and update deleteInquiry and updateInquiry functions for customer and admin roles

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import Inquiry from "../models/inquiry.js";
-import { isItCustomer } from "./userController.js";
+import { isItAdmin, isItCustomer } from "./userController.js";
 
 export async function addInquiry(req, res) {
   try {
@@ -31,4 +31,88 @@ export async function addInquiry(req, res) {
   } catch (error) {
     res.status(500).json({ error: "Failed to add inquiry" });
   }
+}
+
+export async function getInquiries(req, res) {
+  try {
+    if (!isItCustomer(req)) {
+      const inquiries = await Inquiry.find({ email: req.user.email });
+      res.json(inquiries);
+      return;
+    } else if (isItAdmin(req)) {
+      const inquiries = await Inquiry.find();
+      res.json(inquiries);
+      return;
+    }else{
+        res.status(403).json({ message: "You are not authorized to view inquiries" });
+        return;
+    }
+  }catch (error) {
+    res.status(500).json({ error: "Failed to fetch inquiries" });
+  }
+}
+
+export async function deleteInquiry(req, res) {
+    try {
+        if (!isItAdmin(req)) {
+            const id = req.params.id;
+
+            await Inquiry.deleteOne({ id: id });
+            res.json({ message: "Inquiry deleted successfully" });  
+            return;
+       }else if (isItCustomer(req)) {
+            const id = req.params.id;
+
+            const inquiry = await Inquiry.findOne({ id: id});
+            if(inquiry== null) {
+                res.status(404).json({ message: "Inquiry not found" });
+                return;
+            }else{
+                if(inquiry.email === req.user.email) {
+                    await Inquiry.deleteOne({ id: id });
+                    res.json({ message: "Inquiry deleted successfully" });
+                    return;
+                }
+            }
+            
+        }else{
+            res.status(403).json({ message: "You are not authorized to delete inquiries" });
+        }
+    }catch(error) {
+        res.status(500).json({ error: "Failed to delete inquiry" });
+    }
+}
+
+export async function updateInquiry(req, res) {
+    try {
+        if (isItAdmin(req)) {
+            const id = req.params.id;
+            const data = req.body;
+
+            await Inquiry.updateOne({ id: id },data);
+           res.json({ message: "Inquiry updated successfully" });
+            return;
+        }else if (isItCustomer(req)) {
+            const id = req.params.id;
+            const data = req.body;
+
+            const inquiry = await Inquiry.findOne({ id: id });
+            if (inquiry == null) {
+                res.status(404).json({ message: "Inquiry not found" });
+                return;
+            }else{
+                if (inquiry.email === req.user.email) {
+                    await Inquiry.updateOne({ id: id }, {message: data.message})
+                    res.json({ message: "Inquiry updated successfully" });
+                    return;
+                } else {
+                    res.status(403).json({ message: "You are not authorized to update this inquiry" });
+                    return;
+                }
+            }
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: "Failed to update inquiry" });
+    }
 }
--- routes/inquiryRouter.js
+++ routes/inquiryRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {
-  addInquiry
+  addInquiry,
+  deleteInquiry,
+  getInquiries
 } from "../controllers/inquiryController.js";
 
 const inquiryRouter = express.Router();
 inquiryRouter.post("/", addInquiry);
+inquiryRouter.get("/", getInquiries);
+inquiryRouter.delete("/:id", deleteInquiry);
 
 export default inquiryRouter;
