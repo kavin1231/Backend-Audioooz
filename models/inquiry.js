cffeabb3fab0406c88c1ca1bb13270f234af582a
kavin1231 <chamathkaveendra@gmail.com>
2025-07-02 01:19:18 +0530

    Refactor inquiry model for consistent formatting and code clarity

--- models/inquiry.js
+++ models/inquiry.js
@@ -5,37 +5,36 @@ const inquirySchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    },
-    email: {
-    type: String,   
-    required: true,
-    },
-    message:{
+  },
+  email: {
     type: String,
     required: true,
-    },
-    phone:{
-        type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-        default: Date.now,
-    },
-    response: {
-        type: String,
-        required: false,
-        default: "",
-    },
-    isResolved: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  response: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  isResolved: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
-    })
+const Inquiry = mongoose.model("Inquiry", inquirySchema);
 
-    const Inquiry = mongoose.model("Inquiry", inquirySchema);
-
-    export default Inquiry;
+export default Inquiry;
