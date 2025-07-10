74e1a84dc4300d488d21b69164d106e548b81e89
kavin1231 <chamathkaveendra@gmail.com>
2025-07-10 13:07:18 +0530

    Add order management functionality with routes, controller, and model

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv";
 import reviewRouter from "./routes/reviewRouter.js";
 import inquiryRouter from "./routes/inquiryRouter.js";
 import cors from "cors";
+import orderRouter from "./routes/orderRouter.js";
 dotenv.config();
 
 const app = express();
@@ -41,6 +42,7 @@ app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/reviews", reviewRouter);
 app.use("/api/inquiries", inquiryRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(3005, () => {
   console.log("Server is runing on port 3005");
