528bbb7ab32c819d519b8ab273c3673bd60c68da
kavin1231 <chamathkaveendra@gmail.com>
2025-07-12 17:23:00 +0530

    Fix route order in userRouter to correctly handle GET requests for a single user and all users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,14 +4,14 @@ import {
   registerUser,
   loginUser,
   getUser,
-  getAllUsers
+  getAllUsers,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/:id", getUser); // GET single user by ID
-router.get("/", getAllUsers); // GET all users
+router.get("/", getUser); // GET single user by ID
+router.get("/all", getAllUsers); // GET all users
 
 export default router;
